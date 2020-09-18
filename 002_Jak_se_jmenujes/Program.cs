3cacbb1ce47c68cfd86da3ff70c6e1696e784571
Jan Koupil <jan.koupil@delta-skola.cz>
2020-09-18 11:13:40 +0200

    Add project files.

---
+++ 002_Jak_se_jmenujes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _002_Jak_se_jmenujes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Jak se jmenuješ?"); // vypíšu výzvu
+
+            string name; // oznámím počítači, že budu používat proměnnou name
+                         // a bude to řetězec (kus textu) - string
+
+            name = Console.ReadLine(); // načtení z klávesnice a uložení do name
+
+            Console.WriteLine("Hello " + name + "!"); // slepení řetězců operátorem +
+
+            Console.ReadKey();
+        }
+    }
+}
