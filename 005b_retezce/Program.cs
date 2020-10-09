74d09ec80d0dd6decce72e77407da0d46f64b070
Jan Koupil <jan.koupil@delta-skola.cz>
2020-10-09 13:19:50 +0200

    Lekce 9.10.

---
+++ 005b_retezce/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _005b_retezce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jmeno = "Franta";
+            int pocet = 4;
+
+            Console.WriteLine(jmeno + " má " + pocet + " kočky.");
+            Console.WriteLine("{0} má {1} kočky.", jmeno, pocet);
+            Console.WriteLine($"{jmeno} má {pocet} kočky.");
+        }
+    }
+}
