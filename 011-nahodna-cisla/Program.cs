79d79cae6a0e73b10632afd84fb1cdb1a023b449
Jan Koupil <jan.koupil@gmail.com>
2020-10-12 12:53:50 +0200

    Lekce 12. 10 2020 - Náhodná čísla a podmínky

---
+++ 011-nahodna-cisla/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _011_nahodna_cisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            int cislo = generator.Next(1, 11);
+            Console.WriteLine(cislo);
+
+            int stav = generator.Next(0, 2);           
+            if (stav == 0)
+                Console.WriteLine("Panna");
+            else
+                Console.WriteLine("Vorel");
+        }
+    }
+}
