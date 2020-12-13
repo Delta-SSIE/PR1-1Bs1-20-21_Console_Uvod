d6cd6224fd8cf9c40634a69c470fd39947033137
Jan Koupil <jan.koupil@gmail.com>
2020-12-13 19:00:40 +0100

    Vánoční výzva

---
+++ 040-Vanocni-vyzva-reseni/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _040_Vanocni_vyzva_reseni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zatím nepovím!");
+        }
+    }
+}
