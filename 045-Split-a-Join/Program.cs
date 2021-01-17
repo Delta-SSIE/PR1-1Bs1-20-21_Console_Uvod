81819f05d2283b1d6d759b8b42c63b09ef2bef56
Jan Koupil <jan.koupil@gmail.com>
2021-01-17 22:30:13 +0100

    Demo - pole Split, Join

---
+++ 045-Split-a-Join/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _045_Split_a_Join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = { 1, 5, 10, 17, 26 };
+            //string spojeno = string.Join("|", cisla);
+            string text = "Eva, Marie, Hana, Kateřina";
+
+            string[] jmena = text.Split(", ");
+
+            for (int i = 0; i < jmena.Length; i++)
+            {
+                Console.WriteLine(jmena[i]);
+            }
+
+        }
+    }
+}
---
+++ 046-Pole-Paralelni-pole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _046_Pole_Paralelni_pole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] platy =
+            {
+                26800,
+                13200,
+                33000,
+                65000,
+            };
+
+            string[] jmena =
+            {
+                "Novotný",
+                "Novák",
+                "Procházka",
+                "Černý"
+            };
+
+            for (int i = 0; i < platy.Length; i++)
+            {
+                Console.WriteLine($"Pan {jmena[i]} bere měsíčně {platy[i]}.");
+            }
+
+            string hledany = "Procházka";
+            for (int i = 0; i < platy.Length; i++)
+            {
+                if (jmena[i] == hledany)
+                    Console.WriteLine($"Plat je {platy[i]}.");
+            }
+
+
+        }
+    }
+}
